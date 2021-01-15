e9d76265dc7a18e149b1fe177d4598a44a5e1ab9
arjungarg07 <arjun.garg.cse01@gmail.com>
2021-01-15 18:39:40 +0530

    updated

--- src/connector.js
+++ src/connector.js
@@ -16,4 +16,30 @@ con.connect(function (err) {
     else return console.log("connection establish with Datebase!!!!");
 });
 
+const refreshAll = async () => {
+
+    await connection.query("DROP TABLE IF EXISTS Recipes;",
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+        });
+
+    await connection.query("CREATE TABLE Recipes (id INT AUTO_INCREMENT PRIMARY KEY,title VARCHAR(255) NOT NULL,description VARCHAR(255),category VARCHAR(255), ingredients VARCHAR(255));",
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+        });
+
+    await data.forEach(element => {
+        connection.query("INSERT INTO Recipes (title, description, category, ingredients) VALUES (?,?,?,?);", [element.title, element.description, element.category, element.ingredients], (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+        })
+    });
+}
+refreshAll();
+
 module.exports = con;
